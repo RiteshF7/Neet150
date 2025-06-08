cfd450616036252f050b8386aa40213fc6211079
ritesh <riteshf7@gmail.com>
2025-06-08 10:40:16 +0530

    temp

--- src/main/kotlin/slidingWindow/BestTimeToBuyStock.kt
+++ src/main/kotlin/slidingWindow/BestTimeToBuyStock.kt
@@ -2,20 +2,25 @@ package org.trex.kotlin.slidingWindow
 
 import org.trex.kotlin.BaseExecutor
 
-class BestTimeToBuyStock:BaseExecutor<IntArray,Int>() {
+
+fun main() {
+    BestTimeToBuyStock().printOutput()
+}
+
+class BestTimeToBuyStock : BaseExecutor<IntArray, Int>() {
     override val input: IntArray
-        get() = intArrayOf(10,1,5,6,7,1)
+        get() = intArrayOf(10, 1, 5, 6, 7, 1)
 
     override fun execute(): Int {
-        var maxProfit = 0
-        var l=0
-        var r=1
-        while (l<r){
-            val currentProfit = input[l]-input[r]
-            if (currentProfit > maxProfit){
-
-            }
-        }
+        var l = 0
+        var r = 1
+//        while (l < input.size && r < input.size) {
+//            print("$l,")
+//            println()
+//            println("$r")
+//            l++
+//            r++
+//        }
         return -1
     }
 }
